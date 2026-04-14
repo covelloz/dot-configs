bc8844ecc6f3f1e92ec8481de53d159e89f0d1ce
Michael Covello <michael.covello@campspot.com>
2026-04-14 15:37:04 -0500

    Fix memory leak in yazi-picker.sh; ensures that yazi pid is terminated on script exit. Also reset lastdir everytime yazi is opened anew.

--- helix/yazi-picker.sh
+++ helix/yazi-picker.sh
@@ -2,37 +2,49 @@
 
 zellij action move-pane left
 
-# path references keep track of where yazi left off
-project_root="$PWD"
+# cache & temp files keep track of where yazi left off
 last_dir_file="${XDG_CACHE_HOME:-$HOME/.cache}/yazi/lastdir"
 cwd_file=$(mktemp)
+chooser_file=$(mktemp)
 
-# cache cleanup when script fully exits
-trap 'rm -f "$cwd_file"; rm -f "$last_dir_file"' EXIT
+# reset cache & yazi pid refs when script starts
+yazi_pid=""
+rm -f "$last_dir_file"
+
+# cleanup cache & yazi process when script exits
+cleanup() {
+    rm -f "$cwd_file" "$chooser_file" "$last_dir_file"
+    [[ -n "$yazi_pid" ]] && kill "$yazi_pid" 2>/dev/null
+}
+trap cleanup EXIT
 
 while true; do
-	# checks lastdir cache & resets if pwd changes
-	stored_root=$(sed -n '1p' "$last_dir_file" 2>/dev/null)
-	stored_last=$(sed -n '2p' "$last_dir_file" 2>/dev/null)
-	if [[ "$stored_root" == "$project_root" && -d "$stored_last" ]]; then
-		last_dir="$stored_last"
-	else
-		last_dir="$project_root"
-	fi
+    # open yazi - checks lastdir cache, if no cache open yazi at pwd
+    last_dir=$(cat "$last_dir_file" 2>/dev/null)
+    [[ -d "$last_dir" ]] || last_dir="$PWD"
+    yazi --chooser-file="$chooser_file" --cwd-file="$cwd_file" "$last_dir" &
 
-	# open yazi at cached dir
-	paths=$(yazi --chooser-file=/dev/stdout --cwd-file="$cwd_file" "$last_dir" | sed 's/^/"/;s/$/"/' | tr '\n' ' ')
+    # store yazi process pid for cleanup (hack to get pid via a blocking background process)
+    yazi_pid=$!
+    wait "$yazi_pid"
+    yazi_pid=""
 
-	# update the lastdir cache
-	new_dir=$(cat "$cwd_file" 2>/dev/null)
-	[[ -d "$new_dir" ]] && printf '%s\n%s\n' "$project_root" "$new_dir" > "$last_dir_file"
+    # formats yazi file(s) output for helix's :open command
+    paths=$(sed 's/^/"/;s/$/"/' "$chooser_file" | tr '\n' ' ')
 
-	# use zellij to open the yazi path ref in helix
-	if [[ -n "$paths" ]]; then
-		zellij action focus-next-pane
-		zellij action write 27 # send <Escape> key
-		zellij action write-chars ":open $paths"
-		zellij action write 13 # send <Enter> key
-		zellij action focus-previous-pane
-	fi
+    # truncates chooser_file between iters
+    > "$chooser_file"
+
+    # update lastdir cache file in .cache/yazi (helpful for debugging)
+    new_dir=$(cat "$cwd_file" 2>/dev/null)
+    [[ -d "$new_dir" ]] && echo "$new_dir" > "$last_dir_file"
+
+    # use zellij to open the yazi path ref in helix
+    if [[ -n "$paths" ]]; then
+        zellij action focus-next-pane
+        zellij action write 27 # send <Escape> key
+        zellij action write-chars ":open $paths"
+        zellij action write 13 # send <Enter> key
+        zellij action focus-previous-pane
+    fi
 done
